a0a25bde65bacbc92aa701475cf8288516f589f6
Jae sung Chung <abattery@gmail.com>
2014-08-18 15:36:33 +0900

    퇴거 정산서 [을]지 입력/수정하기 - UI 코딩 구현

--- buildingApp/static/js/leave_tenant.angular.js
+++ buildingApp/static/js/leave_tenant.angular.js
@@ -2,6 +2,124 @@ angular.module('yewooApp', [])
     .controller('MainCtrl', function($scope, $timeout) {
 
         var s = $scope;
+        var payments = [];
+        var paymentDetails = [];
+
+        // 미납요금 내역 가져오기
+
+        function AdjustPaymentYM(nowym, y, m, type)
+        {
+            nowy = Number(nowym.split('.')[0].trim());
+            nowm = Number(nowym.split('.')[1].trim());
+            if (type == 'min') {
+                if (y < nowy || (y == nowy && m < nowm))
+                    return y+'.'+m;
+            }
+            else {
+                if (y > nowy || (y == nowy && m > nowm))
+                    return y+'.'+m;
+            }
+            return nowym;
+        }
+
+        var doAjaxAllList = function() {
+            var postData = {};
+            var csrftoken = $.cookie('csrftoken');
+            postData['csrfmiddlewaretoken'] = csrftoken;
+            postData['building_id'] = $("#bid").val();
+            postData['resident_id'] = $("#rid").val();
+            postData['year'] = new Date().getFullYear();
+            postData['month'] = new Date().getMonth() + 1;
+            postData['is_empty'] = false;
+
+            $.ajax({
+                type : 'POST',
+                url : '/lease/payment/detail/getAllInfo/',
+                data : postData,
+                success : function(result) {
+                    var today = Number(new Date().getDate());
+                    var thisYear = result[0].year;
+                    var thisMonth = result[0].month;
+                    var thisNumber = result[0].number;
+
+                    // 연체리스트, 수정사항리스트 두 가지로 분리하기
+                    for (i = 0; i < result.length; i++) {
+                        if (result[i].type == 'basic') {
+                            // 만약 이번 달 납부 내역이 아직 납부일 전이거나 완납한 경우에는 '이번 달 리스트'에 따로 담아야 한다.
+                            if (result[i].number == thisNumber && 
+                                (today <= Number(result[i].leasePayDate) || (result[i].payStatus == -1 && result[i].payDateDay <= Number(result[i].leasePayDate))) ) {
+                                while (result[i].number == thisNumber) {
+                                    result[i].isThis = Number(1);
+                                    payments_thisMonth.push(result[i]);
+                                    payments.push(result[i]);
+                                    i++;
+                                }
+                                i--;
+                            }
+                            else {
+                                result[i].isThis = Number(0);
+                                payments.push(result[i]);
+                            }
+                        }
+                        else 
+                            paymentDetails.push(result[i]); 
+                    }
+
+                    // 미납 년/월 범위 구하기, 현재 미납 달 횟수 구하기
+                    var minym = '9999.12', maxym = '0.0', payCnt = 0;
+                    var totalAmountNoPay = 0;
+                    for (i = 0; i < payments.length; i++) {
+                        if (payments[i].payStatus != -1 && payments[i].isThis == 0) {
+                            // 직전 항목과 고지회차가 다를 때만 미납액/연체료 계산을 해야 한다.
+                            if (i == 0 || Number(payments[i-1].number) != Number(payments[i].number)) {
+                                minym = AdjustPaymentYM(minym, payments[i].year, payments[i].month, 'min');
+                                maxym = AdjustPaymentYM(maxym, payments[i].year, payments[i].month, 'max');
+                                payCnt++;
+                                totalAmountNoPay += Number(payments[i].amountNoPay);
+                            }
+                        }
+                    }
+                    if (minym == '9999.12') minym = '없음';
+                    if (maxym == '0.0') maxym = '없음';
+            
+                    /*
+                    [
+                        { checked: false, amount: 10000, month: "2014/03", time: 10, paidDate: new Date(), expectedDate: new Date(), revisiedAmount: 10000 },
+                        { checked: false, amount: 20000, month: "2013/03", time: 11, paidDate: new Date(), expectedDate: new Date(), revisiedAmount: 20000 },
+                        { checked: false, amount: 30000, month: "2012/03", time: 12, paidDate: new Date(), expectedDate: new Date(), revisiedAmount: 30000 }
+                    ]
+                    */
+                    
+                    var cases = [];
+                    var lastPayment = null;
+                    
+                    for (var i = 0; i < payments.length; i++) {
+                        var payment = payments[i];
+                        //if (payment.isThis == 1 || payment.payStatus == -1) continue;
+                        payment.amount = payment.amountNoPay;
+                        payment.revisiedAmount = payment.amountNoPay;
+                        payment.expectedDate = new Date(payment.year, payment.month);
+                        payment.isPaid = payment.payStatus == -1 || (i > 0 && payment.number == payments[i - 1].number);
+                        
+                        cases.push(payment);
+                    }
+
+                    s.$apply(function () {
+                        s.cases = cases;
+                    });
+                    
+                    // 납부 상세 리스트 보여주기
+                    //var template = new EJS({url : '/static/ejs/03_03_payment_detail_tab1_list.ejs'}).render({'data' : payments, 'minym' : minym, 'maxym' : maxym, 'payCnt' : payCnt, 'totalAmountNoPay' : totalAmountNoPay});
+                    //$('#payment_list').html(template);
+                },
+
+                error : function(msg) {
+                    alert('데이터를 로딩하지 못했습니다...\n페이지 새로고침을 해 보시기 바랍니다.');
+                },
+            });
+        }
+
+        doAjaxAllList();
 
         s.mode = 2;
 
@@ -16,33 +134,76 @@ angular.module('yewooApp', [])
         s.account = ""; // 계좌번호
 
         s.unpaidClosed = false;
-        s.unpaidExtraTypes = [ 
-            { id:1, title: "미납 요금 및 공과금", subTypes: [ "임대료 및 관리비", "공과금" ], hasYear:true, hasMonth: true, amount: 0 },
-            { id:2, title: "청소비", amount: 50000 },
-            { id:3, title: "보증금 반환", isReturn: true, amount: 0 },
-            { id:4, title: "Card-Key 보증금", isReturn: true, amount: 20000 },
-            { id:5, title: "중도해지 수수료", amount: 0 },
-            { id:6, title: "추가사항", amount:0 }
+
+        // MoneyChanges
+        s.moneyChangeTypes = [ 
+            { id:1, title: "선불제", subTypes: [ "입금", "입금대기", "출금", "출금대기", "입금완료", "출금완료" ], hasDate: true, amount: 0 },
+            { id:2, title: "임대계약금", subTypes: [ "입금", "입금대기", "출금", "출금대기", "입금완료", "출금완료" ], hasDate: true, amount: 0 },
+            { id:3, title: "중도금", subTypes: [ "입금", "입금대기", "출금", "출금대기", "입금완료", "출금완료" ], hasDate: true, amount: 0 },
+            { id:4, title: "임대 보증금 잔금", subTypes: [ "입금", "입금대기", "출금", "출금대기", "입금완료", "출금완료" ], hasDate: true, amount: 0 },
         ];
 
-        s.extraCost = { type: s.unpaidExtraTypes[0], desc: "", amount: 0, subType: s.unpaidExtraTypes[0].subTypes ? s.unpaidExtraTypes[0].subTypes[0] : null, month: null, year: s.unpaidExtraTypes[0].hasYear ? new Date().getFullYear() : null, month: s.unpaidExtraTypes[0].hasMonth ? new Date().getMonth() + 1 : null };
-
-        s.updateExtraType = function() {
-            if (!s.extraCost.type.subTypes) { s.extraCost.subType = null; }
-            else { s.extraCost.subType = s.extraCost.type.subTypes[0]; }
-            if (!s.extraCost.type.hasMonth) { s.extraCost.month = null; }
-            else { s.extraCost.month = new Date().getMonth() + 1; } 
-            if (!s.extraCost.type.hasYear) { s.extraCost.year = null; }
-            else { s.extraCost.year = new Date().getFullYear(); } 
-            s.extraCost.amount = s.extraCost.type.amount;
+        s.updateMoneyChangeType = function() {
+            if (!s.moneyChange.type.subTypes) { s.moneyChange.subType = null; }
+            else { s.moneyChange.subType = s.moneyChange.type.subTypes[0]; }
+            if (!s.moneyChange.type.hasMonth) { s.moneyChange.month = null; }
+            else { s.moneyChange.month = new Date().getMonth() + 1; } 
+            if (!s.moneyChange.type.hasYear) { s.moneyChange.year = null; }
+            else { s.moneyChange.year = new Date().getFullYear(); } 
+            s.moneyChange.amount = s.moneyChange.type.amount;
+            s.moneyChange.date = new Date().getFullYear() + "." + (new Date().getMonth() + 1) + "." + (new Date().getDate());
         }
 
-        s.updateExtraType();
+        var moneyChangeType = s.moneyChangeTypes[0];
+        s.moneyChange = { type: moneyChangeType, desc: "", amount: 0 };
 
-        s.extraCosts = [];
+        s.updateMoneyChangeType();
 
-        s.addExtraCost = function(cost) {
-            s.extraCosts.push({
+        s.moneyChanges = [];
+
+        s.addMoneyChange = function(cost) {
+            s.moneyChanges.push({
+                type: cost.type,
+                desc: cost.desc,
+                amount: cost.amount,
+                subType: cost.subType,
+                month: cost.month,
+                year: cost.year,
+                date: $(".datepicker").val(),
+                checked: true
+            });
+        }
+
+        s.removeMoneyChange = function(cost) {
+            s.moneyChanges = _.filter(s.moneyChanges, function(a) { return a != cost; });
+        }
+
+        // Records
+        s.recordTypes = [ 
+            { id:1, title: "전기", amount: 0 },
+            { id:2, title: "수도", amount: 0 },
+            { id:3, title: "가스", amount: 0 }
+        ];
+
+        s.updateRecordType = function() {
+            if (!s.record.type.subTypes) { s.record.subType = null; }
+            else { s.record.subType = s.record.type.subTypes[0]; }
+            if (!s.record.type.hasMonth) { s.record.month = null; }
+            else { s.record.month = new Date().getMonth() + 1; } 
+            if (!s.record.type.hasYear) { s.record.year = null; }
+            else { s.record.year = new Date().getFullYear(); } 
+            s.record.amount = s.record.type.amount;
+        }
+
+        var recordType = s.recordTypes[0];
+        s.record = { type: recordType, desc: "", amount: 0 };
+
+        s.updateRecordType();
+
+        s.records = [];
+
+        s.addRecord = function(cost) {
+            s.records.push({
                 type: cost.type,
                 desc: cost.desc,
                 amount: cost.amount,
@@ -51,126 +212,16 @@ angular.module('yewooApp', [])
                 year: cost.year,
                 checked: true
             });
-            s.updateUnpaid();
         }
 
-        s.removeExtraCost = function(cost) {
-            s.extraCosts = _.filter(s.extraCosts, function(a) { return a != cost; });
-            s.updateUnpaid();
+        s.removeRecord = function(cost) {
+            s.records = _.filter(s.records, function(a) { return a != cost; });
         }
 
-        // 미납 요금 및 공과금 section
-        //s.unpaidTableWidths = new Array(4,4,5,5,4, 14, 7, 4,4, 8,8,8, 4, 3,3,3, 6,6);
-        s.unpaidComputed = 0;
-        s.unpaidDirected = 0;
-        s.unpaidCases = [
-            { checked: false, amount: 10000, month: "2014/03", time: 10, paidDate: new Date(), expectedDate: new Date(), revisiedAmount: 10000 },
-            { checked: false, amount: 20000, month: "2013/03", time: 11, paidDate: new Date(), expectedDate: new Date(), revisiedAmount: 20000 },
-            { checked: false, amount: 30000, month: "2012/03", time: 12, paidDate: new Date(), expectedDate: new Date(), revisiedAmount: 30000 }
-        ]
-
-        s.toggleAllOfUnpaidCases = function() {
-            $timeout(function()
-            {
-                for (var i = 0; i < s.unpaidCases.length; i ++)
-                {
-                    s.unpaidCases[i].checked = s.isAllOfUnpaidCases;
-                }
-                s.updateUnpaidCases();
-                s.updateUnpaid();
-            });
-        }
-
-        s.updateUnpaidCases = function() {
-            $timeout(function()
-            {
-                s.isAllOfUnpaidCases = _.reduce(s.unpaidCases, function (r, unpaidCase) {
-                    if (unpaidCase.checked) return r;
-                    else return false;
-                }, true);
-            });
-        };
-
-        s.isAllOfUnPaidCases = true;
-        s.sumOfExtraCosts = 0;
-        s.updateUnpaid = function () {
-            s.fee = _.reduce(s.feeCosts, function (num, feeCost) {
-                var val = (feeCost.checked ? feeCost.amount : 0);
-                if (feeCost.type.isReturn)
-                    return num - val;
-                else
-                    return num + val;
-            }, 0);
-
-            s.sumOfExtraCosts = _.reduce(s.extraCosts, function (num, extraCost) {
-                if (extraCost.type.isReturn)
-                    return num - extraCost.amount;
-                else
-                    return num + extraCost.amount;
-            }, 0);
-            s.unpaidDirected = _.reduce(s.extraCosts, function (num, extraCost) {
-                var val = (extraCost.checked ? extraCost.amount : 0);
-                if (extraCost.type.isReturn)
-                    return num - val;
-                else
-                    return num + val;
-            }, 0);
-            s.unpaidComputed = _.reduce(s.unpaidCases, function (num, unpaidCase) {
-                var val = (unpaidCase.checked ? unpaidCase.revisiedAmount : 0);
-                return num + val;
-            }, 0);
-            s.unpaid = s.unpaidComputed + s.unpaidDirected;
-            s.updateTotalRefund();
-        };
-
-        // 시설 관리비
-        s.feeClosed = false;
+        // 퇴거 정산 내역 
+        s.allListClosed = false;
         
-        s.feeTypes = [ 
-            { id:1, title: "변기커버", amount: 0 },
-            { id:2, title: "인덕션", amount: 50000 },
-            { id:3, title: "세탁기", amount: 0 },
-            { id:4, title: "가구", amount: 20000 },
-            { id:5, title: "에어컨 리모콘", amount: 0 },
-            { id:6, title: "도배", amount: 0 },
-            { id:7, title: "씽크대", amount: 0 },
-            { id:8, title: "W/C", amount: 0 },
-        ];
-
-        s.feeCost = { type: s.feeTypes[0], desc: "" };
-        s.feeCosts = [];
-
-        s.updateFeeType = function() {
-            if (!s.feeCost.type.subTypes) { s.feeCost.subType = null; }
-            else { s.feeCost.subType = s.extraCost.type.subTypes[0]; }
-            if (!s.feeCost.type.hasMonth) { s.feeCost.month = null; }
-            else { s.feeCost.month = new Date().getMonth() + 1; } 
-            if (!s.feeCost.type.hasYear) { s.feeCost.year = null; }
-            else { s.feeCost.year = new Date().getFullYear(); } 
-            s.feeCost.amount = s.feeCost.type.amount;
-        }
-
-        s.updateFeeType();
-
-        s.addFeeCost = function(cost) {
-            s.feeCosts.push({
-                type: cost.type,
-                desc: cost.desc,
-                amount: cost.amount,
-                subType: cost.subType,
-                month: cost.month,
-                year: cost.year,
-                checked: true
-            });
-            s.updateUnpaid();
-        }
-
-        s.removeFeeCost = function(cost) {
-            s.extraCosts = _.filter(s.extraCosts, function(a) { return a != cost; });
-            s.updateUnpaid();
-        }
-
-
+        // 퇴거 반환금 
         s.updateTotalRefund = function () {
             s.totalRefund = s.deposit - s.unpaid - s.fee;
             s.isMinusTotalRefund = s.totalRefund < 0;
@@ -180,6 +231,6 @@ angular.module('yewooApp', [])
             return s.totalRefund < 0;
         };
 
-        s.updateUnpaid();
         s.updateTotalRefund();
+              
     });
