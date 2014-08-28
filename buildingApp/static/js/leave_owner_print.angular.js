f9bb7697335adcf06bf67de1a7754d8b428b2d23
Jae sung Chung <abattery@gmail.com>
2014-08-28 14:59:40 +0900

    [퇴거 정산서] [갑]지 템플릿 html angular coding

--- buildingApp/static/js/leave_owner_print.angular.js
+++ buildingApp/static/js/leave_owner_print.angular.js
@@ -5,6 +5,71 @@ angular.module('yewooApp', [])
         var payments = [];
         var paymentDetails = [];
 
+        // Sample Data
+
+        s.data = {
+            buildingName: "벨라루체 2",
+            roomNumber: "102 호",
+            name: "강민형",
+            rentStart: new Date(2014, 5, 17),
+            rentEnd: new Date(2015, 5, 18),
+            leaved: new Date(2014, 9, 18),
+            leaveReason: "만기로 퇴실함",
+            rentMethod: "선불제",
+            deposit: 10000000,
+            rent: 250000,
+            returnMoney: 299400,
+            unpaid: 250000,
+            fee: 250000,
+            bank: "국민은행",
+            account: "070-046646-01-034",
+            accountHolder: "강민형",
+            unpaidCollected: 250000,
+            unpaidAdded: 100000,
+            unpaidList: [
+                { month: 6, year: 2014, num: 1, amount: 21000, deposit: 21000, deposited: new Date(2014, 5, 26), defaultAmount: 0, stat: "처리" },
+                { month: 7, year: 2014, num: 1, amount: 21000, deposit: 21000, deposited: new Date(2014, 5, 26), defaultAmount: 0, stat: "처리" },
+                { month: 8, year: 2014, num: 1, amount: 21000, deposit: 21000, deposited: new Date(2014, 5, 26), defaultAmount: 0, stat: "처리" },
+                { month: 9, year: 2014, num: 1, amount: 21000, deposit: 21000, deposited: new Date(2014, 5, 26), defaultAmount: 0, stat: "처리" },
+                { month: 10, year: 2014, num: 1, amount: 21000, deposit: 21000, deposited: new Date(2014, 5, 26), defaultAmount: 0, stat: "처리" },
+                { month: 11, year: 2014, num: 1, amount: 21000, deposit: 21000, deposited: new Date(2014, 5, 26), defaultAmount: 0, stat: "처리" },
+            ],
+            unpaidAddedList: [
+                { title: "청소비", amount: 0 }
+            ],
+            unpaidComment: "기타사유 구구절절",
+            feeList: [
+                { title: "청소비", amount: 0 },
+                { title: "청소비", amount: 0 },
+                { title: "청소비", amount: 0 },
+                { title: "청소비", amount: 0 },
+                { title: "청소비", amount: 0 }
+            ],
+            feeComment: "",
+            contact: {
+                addr: "서울시 강서구 등촌동 639-59번지 벨라루체1",
+                infoList: [
+                    { title: "담당 매니저", value: "한지환 매니저" },
+                    { title: "담당 연락처", value: "02-882-6766" },
+                    { title: "임대 관리팀", value: "02-3661-0880" },
+                    { title: "경비 관리팀", value: "02-8747-4397" },
+                    { title: "시설 관리팀", value: "02-3661-0880" },
+                    { title: "본사 관리팀", value: "02-888-0005" }
+                ],
+                addrs: [ 
+                    { title: "임대 관리팀", value: "주소" },
+                    { title: "본사 관리팀", value: "주소" },
+                ],
+                email: "yewoo21@hanmail.net",
+                doc: {
+                    num: "2014-06-30 문서",
+                    title: "임대료 및 관리비(공과금) 내역 및 입금 계좌 안내",
+                    written: new Date(2014, 5, 30)
+                },
+                name: "(주) 예우 입주민 생활지원 센터"
+            }
+        };
+
         // 미납요금 내역 가져오기
 
         function AdjustPaymentYM(nowym, y, m, type)
